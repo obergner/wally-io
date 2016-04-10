762d9ed2b10db1e331bbf920ac2936bdd61203ff
obergner <olaf.bergner@gmx.de>
2016-04-10 19:46:57 +0200

    T41: Suppress log output during unit tests.

--- src/test/cpp/tests_main.cpp
+++ src/test/cpp/tests_main.cpp
@@ -1,2 +1,12 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#include <boost/log/core.hpp>
+
+int main( int argc, char const** argv )
+{
+    // Disable all logging during unit tests
+    boost::log::core::get( )->set_logging_enabled( false );
+
+    return Catch::Session( ).run( argc, argv );
+}
