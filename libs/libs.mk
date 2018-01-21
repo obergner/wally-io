a6c54ff429919ee1527a4f6f72f9bfd51ce9353c
obergner <olaf.bergner@gmx.de>
2018-01-21 16:21:10 +0100

    Remove paho mqtt library - we used it in integration tests that are now based on python.

--- libs/libs.mk
+++ libs/libs.mk
@@ -10,15 +10,6 @@
 CWD                             := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
 # -------------------------------------------------------------------------------- 
-# Paho-C MQTT client library (Eclipse) used in integration tests
-# -------------------------------------------------------------------------------- 
-
-PAHO_C_URL                      := http://download.eclipse.org/paho/1.1/eclipse-paho-mqtt-c-unix-1.0.3.tar.gz
-PAHO_C_DIR                      := $(CWD)/org.eclipse.paho.mqtt.c
-PAHO_C_LIBS                     := $(PAHO_C_DIR)/lib
-PAHO_C_INC                      := $(PAHO_C_DIR)/include
-
-# -------------------------------------------------------------------------------- 
 # Asio header only
 # -------------------------------------------------------------------------------- 
 
@@ -35,14 +26,4 @@ BA_QUEUE_EXT_INC                := $(BA_QUEUE_EXT_DIR)
 #######################################################################################################################
 # Rules
 #######################################################################################################################
-
-paho-c                          : $(PAHO_C_DIR)
-
-$(PAHO_C_DIR)                   : 
-	@mkdir $(PAHO_C_DIR) > /dev/null 2>&1; \
-		pushd $(PAHO_C_DIR) > /dev/null; \
-		echo "Downloading paho c library from: " $(PAHO_C_URL); \
-		curl --progress-bar $(PAHO_C_URL) | tar -xz; \
-		echo "[DONE]"; \
-		popd > /dev/null;
 
